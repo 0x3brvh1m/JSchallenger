8d0627ddc34d188661c70d79c6673fc61d15a2f6
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-09-13 13:46:53 +0200

    Create Create a range of numbers.js

---
+++ JavaScript Arrays/Create a range of numbers.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (min and max) as arguments
+// Return an array of numbers in the range min to max
+
+function myFunction(min, max) {
+   const x = []
+   for(min ; min <=max ; min++){
+   x.push(min)}
+   return x
+}
