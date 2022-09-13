a248f82e23182632e31be4c3d4b2b8b2def78311
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-09-13 14:13:53 +0200

    Create Merge two arrays with duplicate values.js

---
+++ JavaScript Arrays/Merge two arrays with duplicate values.js
@@ -0,0 +1,9 @@
+// Write a function that takes two arrays as arguments
+// Merge both arrays and remove duplicate values
+// Sort the merge result in ascending order
+// Return the resulting array
+
+function myFunction(a, b) {
+   
+   return [...new Set(a.concat(b).sort((a,b)=>a-b))]
+}
