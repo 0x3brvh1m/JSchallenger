7b66a791cda8e2598e43965472794c2baebb811d
Ebrahim <107937836+0x3brvh1m@users.noreply.github.com>
2022-09-13 13:24:24 +0200

    Create Count number of negative values in array.js

---
+++ JavaScript Arrays/Count number of negative values in array.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array of numbers as argument
+// Return the number of negative values in the array
+
+function myFunction(a) {
+   
+   return a.filter(a => a < 0).length
+}
